7ad56128727b2eec6a01a90d5521ccaae2741834
shejialuo <shejialuo@gmail.com>
2022-04-02 11:54:27 +0800

    Finish 141

---
+++ 141.linked-list-cycle.cpp
@@ -0,0 +1,49 @@
+/*
+ * @lc app=leetcode.cn id=141 lang=cpp
+ *
+ * [141] Linked List Cycle
+ */
+
+#include <iostream>
+using namespace std;
+
+struct ListNode {
+  int val;
+  ListNode *next;
+  ListNode(int x) : val(x), next(NULL) {}
+};
+
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+  // Floyd circle detection
+  bool hasCycle(ListNode *head) {
+    ListNode* slowPointer = head;
+    ListNode* fastPointer = head;
+    if(slowPointer == nullptr) {
+      return false;
+    }
+    while(fastPointer) {
+      fastPointer = fastPointer->next;
+      if(fastPointer != nullptr) {
+        slowPointer = slowPointer->next;
+        fastPointer = fastPointer->next;
+      }
+      if(slowPointer == fastPointer) {
+        return true;
+      }
+    }
+    return false;
+  }
+};
+// @lc code=end
+
