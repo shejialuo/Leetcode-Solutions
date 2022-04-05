259cfc46f0bd75b41a85a15174768ca30128a685
shejialuo <shejialuo@gmail.com>
2022-04-05 19:49:18 +0800

    Finish 26

---
+++ 26.remove-duplicates-from-sorted-array.cpp
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode.cn id=26 lang=cpp
+ *
+ * [26] Remove Duplicates from Sorted Array
+ */
+
+#include <vector>
+using namespace std;
+
+// @lc code=start
+class Solution {
+public:
+  int removeDuplicates(vector<int>& nums) {
+    int i = 0;
+    for(int n: nums) {
+      if(!i || n > nums[i - 1]) {
+        nums[i++] = n;
+      }
+    }
+    return i;
+  }
+};
+// @lc code=end
+
