a0877fe76bab1f9070635e0d60781d53f2968b6a
shejialuo <shejialuo@gmail.com>
2022-04-05 22:54:56 +0800

    Finish 344

---
+++ 344.reverse-string.cpp
@@ -0,0 +1,22 @@
+/*
+ * @lc app=leetcode.cn id=344 lang=cpp
+ *
+ * [344] Reverse String
+ */
+
+#include <vector>
+using namespace std;
+
+// @lc code=start
+class Solution {
+public:
+  void reverseString(vector<char>& s) {
+    int pre = 0;
+    int end = s.size() - 1;
+    while(pre < end) {
+      swap(s[pre++], s[end--]);
+    }
+  }
+};
+// @lc code=end
+
