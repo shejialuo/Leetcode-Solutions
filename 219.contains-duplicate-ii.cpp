efda6121e3dde404f40550d6709578cc0ccc29e5
shejialuo <shejialuo@gmail.com>
2022-04-06 20:02:55 +0800

    Finish 219

---
+++ 219.contains-duplicate-ii.cpp
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode.cn id=219 lang=cpp
+ *
+ * [219] Contains Duplicate II
+ */
+
+#include <vector>
+#include <unordered_map>
+#include <cmath>
+using namespace std;
+
+// @lc code=start
+class Solution {
+public:
+  bool containsNearbyDuplicate(vector<int>& nums, int k) {
+    unordered_map<int, int> hash;
+    for(int i = 0; i < nums.size(); ++i) {
+      if(hash[nums[i]] != 0) {
+        if(abs(i - (hash[nums[i]] - 1 )) <= k) {
+          return true;
+        }
+      }
+      hash[nums[i]] = i + 1;
+    }
+    return false;
+  }
+};
+// @lc code=end
+
