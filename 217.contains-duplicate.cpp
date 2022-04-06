b12ba9030a33bea9cc92ea9b248b301a7b35fa19
shejialuo <shejialuo@gmail.com>
2022-04-06 20:07:35 +0800

    Finish 217

---
+++ 217.contains-duplicate.cpp
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode.cn id=217 lang=cpp
+ *
+ * [217] Contains Duplicate
+ */
+
+#include <vector>
+#include <unordered_set>
+using namespace std;
+
+// @lc code=start
+class Solution {
+public:
+  bool containsDuplicate(vector<int>& nums) {
+    unordered_set<int> hash {};
+    for(int i = 0; i < nums.size(); ++i) {
+      if(hash.count(nums[i]) != 0) {
+        return true;
+      }
+      hash.insert(nums[i]);
+    }
+    return false;
+  }
+};
+// @lc code=end
+
