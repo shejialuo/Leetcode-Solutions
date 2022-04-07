9bdaf95cf376687ccd353b34a1f67340111f0611
shejialuo <shejialuo@gmail.com>
2022-04-07 19:58:03 +0800

    Finish 144

---
+++ 144.binary-tree-preorder-traversal.cpp
@@ -0,0 +1,53 @@
+/*
+ * @lc app=leetcode.cn id=144 lang=cpp
+ *
+ * [144] Binary Tree Preorder Traversal
+ */
+
+#include <vector>
+#include <stack>
+using namespace std;
+
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+  vector<int> preorderTraversal(TreeNode* root) {
+    vector<int> ans {};
+    stack<TreeNode*> st;
+    while(root != nullptr || !st.empty()) {
+      while(root != nullptr) {
+        ans.push_back(root->val);
+        st.push(root);
+        root = root -> left;
+      }
+      if(!st.empty()) {
+        TreeNode* node = st.top();
+        root = node->right;
+        st.pop();
+      }
+    }
+    return ans;
+  }
+};
+// @lc code=end
+
