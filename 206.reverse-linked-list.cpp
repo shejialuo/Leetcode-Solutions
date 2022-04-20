038eb5c92dcf5c14b78ab53280343ab3447f3bf1
shejialuo <shejialuo@gmail.com>
2022-04-20 20:30:05 +0800

    Finish 206

---
+++ 206.reverse-linked-list.cpp
@@ -0,0 +1,44 @@
+/*
+ * @lc app=leetcode.cn id=206 lang=cpp
+ *
+ * [206] Reverse Linked List
+ */
+
+struct ListNode {
+  int val;
+  ListNode *next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int x) : val(x), next(nullptr) {}
+  ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+  ListNode* reverseList(ListNode* head) {
+    if(head == nullptr) return head;
+    ListNode* prev = nullptr;
+    ListNode* next = head;
+
+    while(next != nullptr) {
+      ListNode* temp = next->next;
+      next->next = prev;
+      prev = next;
+      next = temp;
+    }
+
+    return prev;
+  }
+};
+// @lc code=end
+
