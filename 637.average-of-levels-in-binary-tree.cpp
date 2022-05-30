64deac34134c5bebb5c18b1338fae4b53705fc75
shejialuo <shejialuo@gmail.com>
2022-05-30 22:20:15 +0800

    Finish 637

---
+++ 637.average-of-levels-in-binary-tree.cpp
@@ -0,0 +1,59 @@
+/*
+ * @lc app=leetcode.cn id=637 lang=cpp
+ *
+ * [637] Average of Levels in Binary Tree
+ */
+
+#include <vector>
+#include <queue>
+using namespace std;
+
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+  vector<double> averageOfLevels(TreeNode* root) {
+    queue<TreeNode*> qu;
+    vector<double> ans {};
+    if(root != nullptr) qu.push(root);
+    while(!qu.empty()) {
+      int size = qu.size();
+      long sum = 0;
+      for(int i = 0; i < size; ++i) {
+        TreeNode* node = qu.front();
+        qu.pop();
+        sum += node->val;
+        if(node->left != nullptr) {
+          qu.push(node->left);
+        }
+        if(node->right != nullptr) {
+          qu.push(node->right);
+        }
+      }
+      ans.push_back(static_cast<double>(sum) / static_cast<double>(size));
+    }
+    return ans;
+  }
+
+};
+// @lc code=end
+
