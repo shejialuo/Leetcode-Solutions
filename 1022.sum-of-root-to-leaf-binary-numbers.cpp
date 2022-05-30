213966d30f9f577809ce6bce1c4302cef4b3f25b
shejialuo <shejialuo@gmail.com>
2022-05-30 22:02:08 +0800

    Finish 1022

---
+++ 1022.sum-of-root-to-leaf-binary-numbers.cpp
@@ -0,0 +1,49 @@
+/*
+ * @lc app=leetcode.cn id=1022 lang=cpp
+ *
+ * [1022] Sum of Root To Leaf Binary Numbers
+ */
+
+#include <string>
+#include <cmath>
+using namespace std;
+
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+private:
+  int helper(TreeNode* node, int val) {
+    if (node == nullptr) return 0;
+
+    val = (val << 1) | node->val;
+    if(node->left == nullptr && node->right == nullptr) {
+        return val;
+    }
+    return helper(node->left, val) + helper(node->right, val);
+  }
+public:
+  int sumRootToLeaf(TreeNode* root) {
+    return helper(root, 0);
+  }
+};
+// @lc code=end
+
