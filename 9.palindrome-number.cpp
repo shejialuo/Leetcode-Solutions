1bbd8d0b4e7e8775d55fdb54a7e5fd6e13501fe1
shejialuo <shejialuo@gmail.com>
2022-06-07 11:10:32 +0800

    Finish 9

---
+++ 9.palindrome-number.cpp
@@ -0,0 +1,21 @@
+/*
+ * @lc app=leetcode.cn id=9 lang=cpp
+ *
+ * [9] Palindrome Number
+ */
+
+// @lc code=start
+class Solution {
+public:
+  bool isPalindrome(int x) {
+    if(x < 0 || (x!=0 && x % 10==0)) return false;
+    int n = 0;
+    while(x > n) {
+      n = n * 10 + x % 10;
+      x /= 10;
+    }
+    return (x==n)||(x==n/10);
+  }
+};
+// @lc code=end
+
