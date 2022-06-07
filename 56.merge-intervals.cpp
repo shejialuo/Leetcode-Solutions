a370c8d8f05b323b803666d0e2068b3b71cb8d0e
shejialuo <shejialuo@gmail.com>
2022-06-07 10:47:57 +0800

    Finish 56

---
+++ 56.merge-intervals.cpp
@@ -0,0 +1,39 @@
+/*
+ * @lc app=leetcode.cn id=56 lang=cpp
+ *
+ * [56] Merge Intervals
+ */
+
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+// @lc code=start
+class Solution {
+public:
+  vector<vector<int>> merge(vector<vector<int>>& intervals) {
+    vector<vector<int>> ans{};
+    sort(intervals.begin(), intervals.end(),
+      [](const vector<int>& interval1, const vector<int>& interval2) {
+        return interval1[0] < interval2[0];
+      });
+    for(vector<int>& interval: intervals) {
+      if(ans.empty()) {
+        ans.push_back(interval);
+      } else {
+        vector<int>& current = ans.back();
+        if(interval[0] > current[1]) {
+          ans.push_back(interval);
+        } else if(interval[1] <= current[1]) {
+          continue;
+        } else {
+          current[1] = interval[1];
+        }
+      }
+    }
+
+    return ans;
+  }
+};
+// @lc code=end
+
