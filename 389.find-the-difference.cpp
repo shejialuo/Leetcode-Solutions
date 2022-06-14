e34f9b7b7d24fa7e3ae36ed79d75b3220c1e57e2
shejialuo <shejialuo@gmail.com>
2022-06-14 23:13:05 +0800

    Finsh 389

---
+++ 389.find-the-difference.cpp
@@ -0,0 +1,22 @@
+/*
+ * @lc app=leetcode.cn id=389 lang=cpp
+ *
+ * [389] Find the Difference
+ */
+
+#include <string>
+using namespace std;
+
+// @lc code=start
+class Solution {
+public:
+  char findTheDifference(string s, string t) {
+    char ans {};
+    for(size_t i = 0; i < s.size(); ++i) {
+      ans ^= s[i] ^ t[i];
+    }
+    return ans ^ t.back();
+  }
+};
+// @lc code=end
+
