044751c4d61b82808f5517367ffb83b43fc71fe2
shejialuo <shejialuo@gmail.com>
2022-06-24 10:31:40 +0800

    Finish 917

---
+++ 917.reverse-only-letters.cpp
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode.cn id=917 lang=cpp
+ *
+ * [917] Reverse Only Letters
+ */
+
+#include <string>
+#include <cctype>
+#include <iostream>
+using namespace std;
+
+// @lc code=start
+class Solution {
+public:
+  string reverseOnlyLetters(string s) {
+    int i = 0, j = s.size() - 1;
+    while(i < j) {
+      while(j >= 0 && !isalpha(s[j])) j--;
+      while(i < s.size() && !isalpha(s[i])) i++;
+      if(i < j) {
+        swap(s[i], s[j]);
+      }
+      i++; j--;
+    }
+    return s;
+  }
+};
+// @lc code=end
