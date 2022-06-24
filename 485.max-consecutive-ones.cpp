cee8ffe2651a44a8387095286f32368a06783f87
shejialuo <shejialuo@gmail.com>
2022-06-24 22:59:49 +0800

    Finish 485

---
+++ 485.max-consecutive-ones.cpp
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode.cn id=485 lang=cpp
+ *
+ * [485] Max Consecutive Ones
+ */
+
+#include <vector>
+using namespace std;
+
+// @lc code=start
+class Solution {
+public:
+  int findMaxConsecutiveOnes(vector<int>& nums) {
+    int consecutiveOnes = 0, maxConsecutiveOnes = 0;
+    for(int num: nums) {
+      if(num == 1) {
+        consecutiveOnes++;
+      } else {
+        consecutiveOnes = 0;
+      }
+      if(consecutiveOnes > maxConsecutiveOnes) {
+        maxConsecutiveOnes = consecutiveOnes;
+      }
+    }
+    return maxConsecutiveOnes;
+  }
+};
+// @lc code=end
+
