da84dadbe8966ef4653343d1ad87380ec86f0d94
shejialuo <shejialuo@gmail.com>
2022-07-07 10:29:47 +0800

    Finish 771

---
+++ 771.jewels-and-stones.cpp
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode.cn id=771 lang=cpp
+ *
+ * [771] Jewels and Stones
+ */
+
+#include <string>
+using namespace std;
+
+// @lc code=start
+class Solution {
+public:
+  int numJewelsInStones(string jewels, string stones) {
+    bool hash['z' - 'A' + 1] {};
+    int ans = 0;
+    for(char c: jewels) {
+      hash[c - 'A'] = true;
+    }
+    for(char c: stones) {
+      if(hash[c - 'A']) ans++;
+    }
+    return ans;
+  }
+};
+// @lc code=end
+
