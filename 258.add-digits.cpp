78d0a6f89c9d4355831da670c3899fcec62aa40d
shejialuo <shejialuo@gmail.com>
2022-07-12 21:02:16 +0800

    Finish 258

---
+++ 258.add-digits.cpp
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode.cn id=258 lang=cpp
+ *
+ * [258] Add Digits
+ */
+
+// @lc code=start
+class Solution {
+public:
+  int addDigits(int num) {
+    while(num >= 10) {
+      int n = num;
+      num = 0;
+      while(n) {
+        num += n % 10;
+        n /= 10;
+      }
+    }
+    return num;
+  }
+};
+// @lc code=end
+
