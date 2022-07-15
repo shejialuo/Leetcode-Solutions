8a61718c08e8f72e755b393397338dbfe377e176
shejialuo <shejialuo@gmail.com>
2022-07-15 17:53:48 +0800

    Refactor problem 30

--- 30.substring-with-concatenation-of-all-words.cpp
+++ 30.substring-with-concatenation-of-all-words.cpp
@@ -10,46 +10,42 @@
 #include <unordered_map>
 using namespace std;
 
-// TODO: need to refactor
+/*
+  * For this problem, we should define the window,
+  * the window size should be the words.size() *
+  * word's length. And we just move the two pointer
+  * one by one to solve this problem.
+*/
 
 // @lc code=start
 class Solution {
 public:
   vector<int> findSubstring(string s, vector<string>& words) {
     int stringLength = words[0].size();
-    cout << stringLength << '\n';
     int wholeWindowSize = words.size() * stringLength;
-    cout << wholeWindowSize << '\n';
 
     vector<int> ans {};
 
-    unordered_map<string, int> hash;
+    unordered_map<string, int> hash {};
     for(const auto& word: words) {
       hash[word]++;
     }
 
-    int i = 0;
-    while(i <= static_cast<int>(s.size()) - wholeWindowSize) {
+    for(int i = 0; i <= static_cast<int>(s.size()) - wholeWindowSize; ++i ) {
+      unordered_map<string, int> window {};
       int j = i;
       while(j < i + wholeWindowSize) {
-        string subStr = s.substr(j, stringLength);
-        cout << subStr << '\n';
-        if(hash[subStr]) {
-          hash[subStr]--;
-          j += stringLength;
+        string word = s.substr(j, stringLength);
+        if(hash[word] != 0) {
+          window[word]++;
+          if(window[word] > hash[word]) break;
         } else {
           break;
         }
+        j += stringLength;
         if(j == i + wholeWindowSize) {
           ans.push_back(i);
         }
-      }
-      i++;
-      for(const auto& word: words) {
-        hash[word] = 0;
-      }
-      for(const auto& word: words) {
-        hash[word]++;
       }
     }
     return ans;
