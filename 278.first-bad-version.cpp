4d31caad6f39c80a362d11653d205c1250b418c8
shejialuo <shejialuo@gmail.com>
2022-07-26 17:00:46 +0800

    Finish 278

---
+++ 278.first-bad-version.cpp
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode.cn id=278 lang=cpp
+ *
+ * [278] First Bad Version
+ */
+
+extern bool isBadVersion(int version);
+
+// @lc code=start
+// The API isBadVersion is defined for you.
+// bool isBadVersion(int version);
+
+class Solution {
+public:
+  int firstBadVersion(int n) {
+    int start = 1, end = n;
+    while(start < end) {
+      int mid = (end - start) / 2 + start;
+      if(isBadVersion(mid)) {
+        end = mid;
+      } else {
+        start = mid + 1;
+      }
+    }
+    return start;
+  }
+};
+// @lc code=end
+
