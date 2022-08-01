a74298441649db29f1b5725decb4dc07e6d9d790
shejialuo <shejialuo@gmail.com>
2022-08-01 22:57:08 +0800

    Finish 383

---
+++ 383.ransom-note.cpp
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode.cn id=383 lang=cpp
+ *
+ * [383] Ransom Note
+ */
+
+#include <string>
+using namespace std;
+
+// @lc code=start
+class Solution {
+public:
+  bool canConstruct(string ransomNote, string magazine) {
+    int dict[26] {};
+    for(char c : magazine) {
+      dict[c - 'a']++;
+    }
+    for(char c: ransomNote) {
+      if(dict[c - 'a'] == 0) return false;
+      dict[c - 'a']--;
+    }
+    return true;
+  }
+};
+// @lc code=end
+
