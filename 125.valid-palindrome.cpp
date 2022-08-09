bba92c4d5842502148f05f725484621a507dc4ac
shejialuo <shejialuo@gmail.com>
2022-08-09 14:50:25 +0800

    Refactor 125

--- 125.valid-palindrome.cpp
+++ 125.valid-palindrome.cpp
@@ -14,8 +14,8 @@ public:
   bool isPalindrome(string s) {
     int i = 0, j = s.size() - 1;
     while(i < j) {
-      while(j >= 0 && !isalnum(s[j])) j--;
-      while(i < s.size() && !isalnum(s[i])) i++;
+      while(i < j && !isalnum(s[j])) j--;
+      while(i < j && !isalnum(s[i])) i++;
       if(i < j) {
         if(tolower(s[i]) != tolower(s[j])) {
           return false;
