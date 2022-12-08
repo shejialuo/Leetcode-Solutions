57aff3c86aaebb8a958335efc0a4d746a5c2e420
shejialuo <shejialuo@gmail.com>
2022-12-08 23:42:40 +0800

    Finish 575

---
+++ 575.distribute-candies.cpp
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode.cn id=575 lang=cpp
+ *
+ * [575] Distribute Candies
+ */
+
+#include <unordered_set>
+#include <vector>
+using namespace std;
+
+// @lc code=start
+class Solution {
+public:
+  /**
+   * It is an easy problem. Just abstract the model
+   *
+   * @param candyType
+   * @return int
+   */
+  int distributeCandies(vector<int> &candyType) {
+    unordered_set<int> types{};
+    for (auto &&type : candyType) {
+      types.insert(type);
+    }
+    return types.size() <= candyType.size() / 2 ? types.size() : candyType.size() / 2;
+  }
+};
+// @lc code=end
