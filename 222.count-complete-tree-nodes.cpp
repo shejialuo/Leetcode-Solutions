96c163080010a69297fa61614177980fcf6a9544
shejialuo <shejialuo@gmail.com>
2023-02-17 17:39:19 +0800

    Finish 222

---
+++ 222.count-complete-tree-nodes.cpp
@@ -0,0 +1,54 @@
+/*
+ * @lc app=leetcode.cn id=222 lang=cpp
+ *
+ * [222] Count Complete Tree Nodes
+ */
+
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right)
+      : val(x), left(left), right(right) {}
+};
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+class Solution {
+public:
+  int countNodes(TreeNode *root) {
+    if (root == nullptr) {
+      return 0;
+    }
+
+    int leftDepth = 0, rightDepth = 0;
+
+    for (TreeNode *ptr = root->left; ptr != nullptr; ptr = ptr->left) {
+      leftDepth++;
+    }
+
+    for (TreeNode *ptr = root->right; ptr != nullptr; ptr = ptr->right) {
+      rightDepth++;
+    }
+
+    if (leftDepth == rightDepth) {
+      return (2 << leftDepth) - 1;
+    }
+
+    return 1 + countNodes(root->left) + countNodes(root->right);
+  }
+};
+// @lc code=end
