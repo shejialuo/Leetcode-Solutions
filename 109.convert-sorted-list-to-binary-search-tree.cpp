cbecc63c19d9f76bcefa81c11523e4132c75d76a
shejialuo <shejialuo@gmail.com>
2023-03-01 23:02:57 +0800

    Finish 109

---
+++ 109.convert-sorted-list-to-binary-search-tree.cpp
@@ -0,0 +1,81 @@
+/*
+ * @lc app=leetcode.cn id=109 lang=cpp
+ *
+ * [109] Convert Sorted List to Binary Search Tree
+ */
+
+struct ListNode {
+  int val;
+  ListNode *next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int x) : val(x), next(nullptr) {}
+  ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right)
+      : val(x), left(left), right(right) {}
+};
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+class Solution {
+public:
+  TreeNode *sortedListToBST(ListNode *head) {
+    // End case
+    if (head == nullptr) {
+      return nullptr;
+    }
+
+    if (head->next == nullptr) {
+      TreeNode *node = new TreeNode(head->val);
+      return node;
+    }
+
+    ListNode *slow = head;
+    ListNode *preSlow = nullptr;
+    ListNode *fast = head;
+
+    // Use slow and fast pointer to find the middle.
+    while (fast != nullptr) {
+      fast = fast->next;
+      if (fast != nullptr) {
+        fast = fast->next;
+        preSlow = slow;
+        slow = slow->next;
+      }
+    }
+    TreeNode *node = new TreeNode(slow->val);
+    preSlow->next = nullptr;
+    node->left = sortedListToBST(head);
+    node->right = sortedListToBST(slow->next);
+    return node;
+  }
+};
+// @lc code=end
