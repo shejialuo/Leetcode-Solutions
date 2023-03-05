74cbb546b8849f74bcf2eacca2476e7fe86373e2
shejialuo <shejialuo@gmail.com>
2023-03-05 18:16:11 +0800

    Finish 51

---
+++ 51.n-queens.cpp
@@ -0,0 +1,60 @@
+/*
+ * @lc app=leetcode.cn id=51 lang=cpp
+ *
+ * [51] N-Queens
+ */
+
+#include <string>
+#include <vector>
+using namespace std;
+
+// @lc code=start
+class Solution {
+private:
+  bool isValid(int row, int col, vector<string> &one, int n) {
+
+    for (int i = 0; i < row; i++) {
+      if (one[i][col] == 'Q') {
+        return false;
+      }
+    }
+
+    for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
+      if (one[i][j] == 'Q') {
+        return false;
+      }
+    }
+
+    for (int i = row - 1, j = col + 1; i >= 0 && j < n; i--, j++) {
+      if (one[i][j] == 'Q') {
+        return false;
+      }
+    }
+    return true;
+  }
+  void solveNQueensHelper(int i, int n, vector<vector<string>> &ans,
+                          vector<string> &one) {
+    if (i == n) {
+      ans.push_back(one);
+      return;
+    }
+
+    for (int j = 0; j < n; ++j) {
+      if (isValid(i, j, one, n)) {
+        one[i][j] = 'Q';
+        solveNQueensHelper(i + 1, n, ans, one);
+        one[i][j] = '.';
+      }
+    }
+  }
+
+public:
+  vector<vector<string>> solveNQueens(int n) {
+    vector<vector<string>> ans{};
+    vector<string> one(n, string(n, '.'));
+    solveNQueensHelper(0, n, ans, one);
+
+    return ans;
+  }
+};
+// @lc code=end
