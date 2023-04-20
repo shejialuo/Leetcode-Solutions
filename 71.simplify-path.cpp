f8530b51ffb589f2833abad8c2b20392d2c42a55
shejialuo <shejialuo@gmail.com>
2023-04-20 15:21:12 +0800

    Finish 71

---
+++ 71.simplify-path.cpp
@@ -0,0 +1,56 @@
+/*
+ * @lc app=leetcode.cn id=71 lang=cpp
+ *
+ * [71] Simplify Path
+ */
+
+#include <string>
+
+using namespace std;
+
+// @lc code=start
+class Solution {
+public:
+  string simplifyPath(string path) {
+    string ans{};
+
+    for (int i = 0; i < path.size(); i++) {
+      // find the next '/', if exist
+      // [i, j) would be the next path
+
+      int j = i;
+      while (j < path.size() && path[j] != '/') {
+        j++;
+      }
+
+      string sub = path.substr(i, j - i);
+
+      // There would be situations for multiple '/'
+      // or '.' in this situation we should do nothing
+      if (sub == "." || sub.empty()) {
+        // do nothing
+      } else if (sub == "..") {
+        // handle the ans, just pop it.
+        while (!ans.empty()) {
+          if (ans.back() == '/') {
+            ans.pop_back();
+            break;
+          }
+          ans.pop_back();
+        }
+      } else {
+        ans += "/" + std::move(sub);
+      }
+      i = j;
+    }
+
+    // handle the special case.
+    if (ans.empty()) {
+      ans = "/";
+    }
+
+    return ans;
+  }
+};
+
+// @lc code=end
