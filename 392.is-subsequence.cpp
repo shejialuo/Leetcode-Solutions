988642fe2d6d4cb80f5e650e204dc4fcacb27f0f
shejialuo <shejialuo@gmail.com>
2023-05-11 15:25:04 +0800

    Add dp solution for 392

--- 392.is-subsequence.cpp
+++ 392.is-subsequence.cpp
@@ -6,12 +6,13 @@
 
 #include <iostream>
 #include <string>
+#include <vector>
 using namespace std;
 
 // @lc code=start
 class Solution {
-public:
-  bool isSubsequence(string s, string t) {
+private:
+  bool isSubsequenceTwoPointer(const string &s, const string &t) {
     int p1 = 0, p2 = 0;
 
     for (char c : s) {
@@ -25,5 +26,26 @@ public:
     }
     return true;
   }
+
+  bool isSubsequenceDP(const string &s, const string &t) {
+    vector<vector<int>> dp(s.size() + 1, vector<int>(t.size() + 1, 0));
+
+    for (int i = 0; i < s.size(); i++) {
+      for (int j = 0; j < t.size(); j++) {
+        if (s[i] == t[j]) {
+          dp[i + 1][j + 1] = dp[i][j] + 1;
+        } else {
+          // Here, we can only delete the t, this is the
+          // big difference.
+          dp[i + 1][j + 1] = dp[i + 1][j];
+        }
+      }
+    }
+
+    return dp.back().back() == s.size();
+  }
+
+public:
+  bool isSubsequence(string s, string t) { return isSubsequenceDP(s, t); }
 };
 // @lc code=end
