603402fd790aa911644bc79c050d6581cff9c10e
shejialuo <shejialuo@gmail.com>
2023-07-07 14:03:14 +0800

    Refactor 5

--- 5.longest-palindromic-substring.cpp
+++ 5.longest-palindromic-substring.cpp
@@ -43,7 +43,6 @@ private:
     vector<vector<bool>> dp(s.size(), vector<bool>(s.size(), false));
 
     int begin = 0, length = 1;
-    bool direction = true;
 
     int start = s.size() - 1;
 
@@ -74,7 +73,46 @@ private:
     return s.substr(begin, length);
   }
 
+  /**
+   * @brief We could easily save the space, because in the above
+   * operation, we could only maintain one variable to maintain
+   * the state.
+   *
+   */
+  string longestPalindromeTwoPointer(const string &s) {
+
+    int begin = 0, length = 1;
+
+    int start = s.size() - 1;
+
+    while (start >= 0) {
+      for (int i = 0; i < 2 && start + i < s.size(); i++) {
+        int localStart = start, localEnd = start + i;
+        bool previous = false;
+        while (localStart >= 0 && localEnd < s.size()) {
+          if (localStart == localEnd) {
+            previous = true;
+          } else if (localStart + 1 == localEnd &&
+                     s[localStart] == s[localEnd]) {
+            previous = true;
+          } else {
+            previous = previous && s[localStart] == s[localEnd];
+          }
+
+          if (previous && localEnd - localStart + 1 > length) {
+            begin = localStart;
+            length = localEnd - localStart + 1;
+          }
+          localStart--;
+          localEnd++;
+        }
+      }
+      start--;
+    }
+    return s.substr(begin, length);
+  }
+
 public:
-  string longestPalindrome(string s) { return longestPalindromeDP(s); }
+  string longestPalindrome(string s) { return longestPalindromeTwoPointer(s); }
 };
 // @lc code=end
